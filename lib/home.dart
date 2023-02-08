8860d41c339aadf51071066e989e48e938c84217
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-02-08 17:40:06 +0800

    Layout with SliverList

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,55 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget{
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            leading: Icon(Icons.menu),
+            title: Text('DP Shop'),
+            actions: [
+              Icon(Icons.shopping_bag_outlined),
+            ],
+          ),
+          //SEARCH BAR
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                return const ListTile(
+                  title: Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search here...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  trailing: Icon(Icons.camera_alt),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+          //CATEGORIES
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    Text('Categories'),
+
+                  ],
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'home.dart';
 
 void main() => runApp(
   MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
     home: Home(),
   )
 );
