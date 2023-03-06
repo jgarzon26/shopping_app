3e3272333de4e0934b063363206800df610ec7bf
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-06 11:32:09 +0800

    SettingDemoData

---
+++ lib/demoData/category_data.dart
@@ -0,0 +1,31 @@
+
+const category = [
+  {
+    'name' : 'Sandal',
+    'image' : 'assets/images/1.png',
+  },
+  {
+    'name' : 'Analog Watch',
+    'image' : 'assets/images/2.png',
+  },
+  {
+    'name' : 'HandBag',
+    'image' : 'assets/images/3.png',
+  },
+  {
+    'name' : 'Big Satchel',
+    'image' : 'assets/images/4.png',
+  },
+  {
+    'name' : 'Small Satchel',
+    'image' : 'assets/images/5.png',
+  },
+  {
+    'name' : 'Heels',
+    'image' : 'assets/images/6.png',
+  },
+  {
+    'name' : 'Digital Watch',
+    'image' : 'assets/images/7.png',
+  },
+];
