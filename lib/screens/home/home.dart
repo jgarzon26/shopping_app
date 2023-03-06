2574244884bc3ba6d297b3820dc7389c24764db2
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-06 18:22:33 +0800

    ItemGrid

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/constants.dart';
 import 'package:shopping_app/demoData/category_data.dart';
 import 'package:shopping_app/screens/home/widgets/categories.dart';
+import 'package:shopping_app/screens/home/widgets/item_card.dart';
 import 'package:shopping_app/screens/home/widgets/search_bar.dart';
 import 'package:shopping_app/screens/home/widgets/section_title.dart';
 
@@ -68,7 +69,7 @@ class Home extends StatelessWidget{
           ),
           SliverToBoxAdapter(
             child: Container(
-              padding: const EdgeInsets.only(top: 15,),
+              padding: const EdgeInsets.only(top: 15, bottom: 20),
               decoration: const BoxDecoration(
                 color: kBGColor,
                 borderRadius: BorderRadius.only(
@@ -96,104 +97,22 @@ class Home extends StatelessWidget{
                   ),
                   const SectionTitle(title: 'Best Selling'),
                   GridView.count(
+                    physics: const NeverScrollableScrollPhysics(),
+                    mainAxisSpacing: 20,
                     crossAxisCount: 2,
                     childAspectRatio: 0.68,
                     shrinkWrap: true,
                     padding: const EdgeInsets.only(top: 0,),
-                    children: [
-                      Container(
-                        height: 200,
-                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                        padding: const EdgeInsets.only(left: 10, right: 10, top: 5,),
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                    color: kMainColor,
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  child: Text(
-                                    '-${products[0]['discount']}%',
-                                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.favorite_border,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            InkWell(
-                              onTap: () {},
-                              child: Container(
-                                margin: const EdgeInsets.all(10),
-                                child: Image.asset(
-                                  products[0]['image']!,
-                                  width: 120,
-                                  height: 120,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.only(bottom: 8),
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                '${products[0]['title']}',
-                                style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: kMainColor,
-                                ),
-                              ),
-                            ),
-                            Text(
-                              '${products[0]['subtitle']}',
-                              style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                                fontSize: 15,
-                                color: kMainColor,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '\$${products[0]['price']}',
-                                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: kMainColor,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.shopping_cart_checkout,
-                                    color: kMainColor,
-                                    size: 20,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                    children: List.generate(
+                      products.length, (index) => ItemCard(
+                      title: products[index]['title']!,
+                      subtitle: products[index]['subtitle']!,
+                      price: products[index]['price']!,
+                      discount: products[index]['discount']!,
+                      image: products[index]['image']!,
+                      onTap: () {},
+                     ),
+                    ),
                   ),
                 ],
               ),
@@ -204,6 +123,8 @@ class Home extends StatelessWidget{
     );
   }
 }
+
+
 
 
 
